94c8bf41bec9cf4172a8ebf4ee03c97338955b22
rkorzeniec <r.korzeniec@gmail.com>
2019-08-23 22:59:24 +0200

    define methods on host object

--- lib/simple_encryptor.rb
+++ lib/simple_encryptor.rb
@@ -6,6 +6,31 @@ require 'simple_encryptor/encryptor'
 require 'simple_encryptor/version'
 
 module SimpleEncryptor
-  class Error < StandardError; end
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def attr_encryptable(*attributes)
+      options = attributes.last.is_a?(Hash) ? attributes.pop : {}
+      attributes.each do |attribute|
+        define_method("#{attribute}=".to_sym) do |value|
+          return if value.nil?
+
+          public_send(
+            "encrypted_#{attribute}=".to_sym,
+            Encryptor.encrypt(value, options)
+          )
+        end
+
+        define_method(attribute) do
+          value = public_send("encrypted_#{attribute}".to_sym)
+          Encryptor.decrypt(value, options) if value.present?
+        end
+
+        define_method("#{attribute}?".to_sym) do
+          value = send(attribute)
+          value.respond_to?(:empty?) ? !value.empty? : !!value
+        end
+      end
+    end
+  end
 end
--- test/test_simple_encryptor.rb
+++ test/test_simple_encryptor.rb
@@ -1,4 +1,80 @@
 require 'test_helper'
 
+class User
+  include SimpleEncryptor
+
+  attr_accessor :encrypted_email, :encrypted_password, :encrypted_token
+
+  attr_encryptable :email, secret: 'foo', salt: 'bar'
+  attr_encryptable :password, :token, secret: 'baz', salt: 'zab'
+
+  def initialize(email: nil, password: nil, token: nil)
+    self.email = email
+    self.password = password
+    self.token = token
+  end
+
+  def encrypted_email?
+    email?
+  end
+end
+
+
 describe SimpleEncryptor do
+  describe '.attr_encryptable' do
+    let(:user) { User.new(email: 'john@example.com') }
+
+    it 'defines writer' do
+      assert(user.respond_to?(:email=))
+    end
+
+    it 'defines reader' do
+      assert(user.respond_to?(:email))
+    end
+
+    it 'defines boolean method' do
+      assert(user.respond_to?(:email?))
+    end
+
+    it 'encrypts email correctly' do
+      decrypted_email = SimpleEncryptor::Encryptor.decrypt(
+        user.encrypted_email, secret: 'foo', salt: 'bar'
+      )
+
+      assert_equal('john@example.com', decrypted_email)
+    end
+
+    it 'encrypted does not equal email' do
+      refute_equal('john@example.com', user.encrypted_email)
+    end
+
+    it 'encrypts as string' do
+      assert_kind_of(String, user.encrypted_email)
+    end
+
+    it 'encrypts always with same length' do
+      assert_equal(138, user.encrypted_email.size)
+    end
+
+    it 'decrypts correctly' do
+      assert_equal('john@example.com', user.email)
+    end
+
+    it 'encrypted when value set' do
+      assert(user.email?)
+    end
+
+    it 'encrypted when value set' do
+      user = User.new
+      refute(user.email?)
+    end
+
+    it 'handles multiple encriptions' do
+      user = User.new(password: 'password', token: 'secret')
+      assert(user.password?)
+      assert_equal('password', user.password)
+      assert(user.token?)
+      assert_equal('secret', user.token)
+    end
+  end
 end
